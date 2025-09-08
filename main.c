9eaeb024a74b86ae9f7e29903c0f6945e66af418
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-09-08 15:38:29 +0300

    Add function to read command

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void msh_init(void) {
     printf("\033[38;5;28m");
@@ -9,6 +12,22 @@ void msh_init(void) {
     printf("██║ ╚═╝ ██║██║   ██║   ╚██████╔╝    ███████║██║  ██║\n");
     printf("╚═╝     ╚═╝╚═╝   ╚═╝    ╚═════╝     ╚══════╝╚═╝  ╚═╝\n");
     printf("\033[0m\n");
+}
+
+char* msh_read_cmd() {
+    char* cmd = NULL;
+    size_t buf_size = 0;
+
+    if (getline(&cmd, &buf_size, stdin) == -1) {
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        }
+
+        fprintf(stderr, "\033[1;31mGetline error (%s)\033[0m\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    return cmd;
 }
 
 int main(void) {
