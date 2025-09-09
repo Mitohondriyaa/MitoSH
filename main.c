548f3800da945b2cab2c276363c896d286c8ee5c
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-09-09 10:39:17 +0300

    Add function to execute command

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 
 void msh_init(void) {
@@ -68,6 +71,31 @@ char** msh_parse_cmd(char* cmd) {
     argv[argc] = NULL;
 
     return argv;
+}
+
+int msh_execute_cmd(char** argv) {
+    pid_t pid = fork();
+    int status = 0;
+
+    if (pid == 0) {
+        if (execvp(argv[0], argv) == -1) {
+            fprintf(stderr, "\033[1;31Execution error (%s)\033[0m\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0) {
+        fprintf(stderr, "\033[1;31Process creation error (%s)\033[0m\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else {
+        waitpid(pid, &status, WAIT_MYPGRP);
+
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
+    }
+
+    return -1;
 }
 
 int main(void) {
